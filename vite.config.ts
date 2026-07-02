b04dbe7cd4026877bbb93a70baff3a8f6a64326a
Gersombs <gersombs@gmail.com>
2026-07-01 21:23:58 -0600

    rute fixed

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/camila/', // <-- ¡Agrega esta línea!
+  base: './', 
   plugins: [react()],
   resolve: {
     alias: {
