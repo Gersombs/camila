46738988d7680c9bbef3edec03e14c92ea832fd7
Gersombs <gersombs@gmail.com>
2026-07-01 21:31:19 -0600

    fix: cambiar a HashRouter para github pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,16 @@
-
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom'; 
 import Index from './pages/Index';
 
 const queryClient = new QueryClient();
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
-
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Index />} />
-        </Routes>
-      </BrowserRouter>
+    <HashRouter> {/* <--- Aquí también */}
+      <Routes>
+        <Route path="/" element={<Index />} />
+      </Routes>
+    </HashRouter>
   </QueryClientProvider>
 );
 
