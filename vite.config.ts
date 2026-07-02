22b382536e33b27261669abb879bc36652e8a24e
Gersombs <gersombs@gmail.com>
2026-07-01 21:20:17 -0600

    chore: configurar deploy a github pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/camila/', // <-- ¡Agrega esta línea!
   plugins: [react()],
   resolve: {
     alias: {
